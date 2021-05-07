04048a0ef323a77014bd8f210bfc2f41d4bd0ba0
Lelberto <jsurieux@gmail.com>
2021-05-07 15:39:24 +0200

    Add Guirlande creation if not exists Add Guirland access middleware

--- src/services/guirlande-service.ts
+++ src/services/guirlande-service.ts
@@ -1,3 +1,5 @@
+import { NextFunction, Request, Response } from 'express';
+import { Access } from '../models/guirlande-model';
 import Service from './service';
 import ServiceContainer from './service-container';
 
@@ -15,5 +17,54 @@ export default class GuirlandeService extends Service {
    */
   public constructor(container: ServiceContainer) {
     super(container);
+  }
+
+  /**
+   * Creates a new Guirlande entry if not exists.
+   * 
+   * This method is called at startup.
+   */
+  public async createIfNotExists(): Promise<void> {
+    try {
+      if (await this.db.guirlande.countDocuments() === 0) {
+        const guirlande = await this.db.guirlande.create({});
+        this.logger.info(`Guirlande entry is created with ID "${guirlande.id}"`);
+      }
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
+
+  /**
+   * Access to Guirlande.
+   * 
+   * An access to Guirlande is valid if :
+   * - The user is authenticated
+   * - The Guirlande access is PUBLIC and the code provided in Access-Code header is valid
+   * 
+   * This method is a handler.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @param next Next handler
+   * @async
+   */
+  public async accessHandler(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const guirlande = await this.container.db.guirlande.findOne();
+      if (res.locals.authUser == null) {
+        if (guirlande.access === Access.PRIVATE) {
+          return res.status(403).send(this.container.errors.formatErrors({ error: 'access_denied', error_description: 'Guirlande access is private' }))
+        }
+        console.log(req.headers)
+        if (req.headers['access-code'] !== guirlande.code) {
+          return res.status(403).send(this.container.errors.formatErrors({ error: 'access_denied', error_description: 'Invalid code' }));
+        }
+      }
+      return next();
+    } catch (err) {
+      this.logger.error(err);
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
   }
 }
